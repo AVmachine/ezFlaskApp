103c7cc738acea0e662e9a567dd140ed4a2b33eb
Andrew Tomich <johnatomich@gmail.com>
2020-11-03 18:48:13 -0600

    Added graph and changed to final_df.csv

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
-import df
+from flask import Flask, render_template, Response
+import df, io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import pylab
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def get_data():
     dataframe = df.test_df()
-
     return render_template('index.html', dataframe=dataframe)
 
 
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    dataframe = df.test_df()
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.hist(dataframe['averageRating'])
+    return fig
 
 
 if __name__ == '__main__':
--- df.py
+++ df.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-def test_df():
-    dataframe = pd.read_csv('test_df.csv')
-    return dataframe
 
+def test_df():
+    dataframe = pd.read_csv('final_df.csv')
+    df = dataframe.sort_values('numVotes', ascending=False).reset_index()
+    df = df.drop('index', axis=1)
+    return df
 
